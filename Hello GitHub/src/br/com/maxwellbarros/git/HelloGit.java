9f0f41d851b5eba4346bd5a7c5865323a2c71db2
maxwellbarros <66340684+maxwellbarros@users.noreply.github.com>
2020-06-05 15:58:29 -0300

    Adicionei arquivos via uploads

---
+++ Hello GitHub/src/br/com/maxwellbarros/git/HelloGit.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package br.com.maxwellbarros.git;
+
+/**
+ * @author maxwellbarros
+ * Exemplo de integração do Eclipse com o GitHub
+ */
+public class HelloGit {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+
+	}
+
+}
